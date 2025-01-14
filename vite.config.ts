0fda03c33c832cba900c2d09c91dd47aed524424
Sankarkumar K <59822719+sankarkumark34@users.noreply.github.com>
2025-01-14 21:47:54 +0530

    chore: Update package configuration and dependencies
    - Renamed package to 'react-toast-notify-ks' and updated version to 1.0.1 in package.json and package-lock.json.
    - Added new devDependencies: '@types/node' and 'vite-plugin-dts'.
    - Enhanced TypeScript configuration with stricter settings and output directory for declarations.
    - Adjusted module resolution strategy in tsconfig.node.json for better compatibility.
    - Updated Vite configuration to include URL handling for improved module imports.

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
 import { resolve } from 'path'
-
+import { fileURLToPath, URL } from 'url'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
